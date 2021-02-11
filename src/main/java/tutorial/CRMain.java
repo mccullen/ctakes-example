36d948e4b8a35dfac4364203621ac00c195213e7
mccullen <mccullen_j@lynchburg.edu>
2021-02-11 16:50:00 -0500

    first commit

---
+++ src/main/java/tutorial/CRMain.java
@@ -0,0 +1,28 @@
+package tutorial;
+
+import org.apache.ctakes.core.config.ConfigParameterConstants;
+import org.apache.ctakes.core.pipeline.PipelineBuilder;
+import org.apache.ctakes.core.pipeline.PiperFileReader;
+
+public class CRMain {
+    public static final String PIPER_FILE = "C:/root/vdt/icapa/nlp/apache-ctakes-4.0.0/resources/org/apache/ctakes/clinical/pipeline/DefaultFastPipeline.piper";
+    public static final String OUTPUT_DIR = "C:/root/";
+    public static final String UMLS_KEY = "08cde565-a6b0-4a50-8035-1a3d6ceb3835";
+
+    public static void main(String[] args) throws Exception {
+        PiperFileReader piperReader = new PiperFileReader();
+        PipelineBuilder builder = piperReader.getBuilder();
+
+        // Set the CLI parameters
+        builder.set("umlsKey", UMLS_KEY);
+        builder.set(ConfigParameterConstants.PARAM_PIPER, PIPER_FILE);
+
+        // Add the default CC
+        builder.writeXMIs(OUTPUT_DIR);
+
+        // Load the piper file line by line
+        piperReader.loadPipelineFile(PIPER_FILE);
+
+        builder.run();
+    }
+}
