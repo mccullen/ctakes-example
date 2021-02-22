7a6c7702d7d371c20aa45ee497bc1ce81521c6bf
mccullen <mccullen_j@lynchburg.edu>
2021-02-22 15:32:23 -0500

    Update pom with fewer dependencies

--- src/main/java/tutorial/Main.java
+++ src/main/java/tutorial/Main.java
@@ -1,21 +1,27 @@
 package tutorial;
 
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.fit.factory.AggregateBuilder;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.jcas.JCas;
+
 import java.io.File;
+import java.io.FileWriter;
 import java.io.InputStream;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        //InputStream input = Util.class.getClassLoader().getResourceAsStream("config.properties");
-        String filePath = Util.class.getClassLoader().getResource("config.properties").getFile();
-        System.out.println("File path: " + filePath);
-        File file = new File(filePath);
-        if (file != null) {
-            System.out.println("Found input from class loader");
-        }
-        File f = new File("src/main/resources/config.properties");
-        if (f != null) {
-            System.out.println("Found f from File");
-            System.out.println(f.toString());
-        }
+
+        JCas jCas = JCasFactory.createJCas();
+        jCas.setDocumentText("");
+        AggregateBuilder aggregateBuilder = new AggregateBuilder();
+        AnalysisEngineDescription aeDescription = AnalysisEngineFactory.createEngineDescription(
+            RegexAnnotator.class,
+            RegexAnnotator.PARAM_REGEX,
+            "trauma|loss of consciousness"
+        );
+        aggregateBuilder.add(aeDescription);
+        aggregateBuilder.createAggregateDescription().toXML(new FileWriter("temp.xml"));
     }
 }
